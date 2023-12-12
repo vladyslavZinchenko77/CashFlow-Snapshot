c55f472076ab976dbc79c86342ad8cf10baa8195
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2023-12-12 21:00:56 +0200

    added header for landing page

--- frontend/src/components/ui/Landing/ui/HeaderLanding/HeaderLanding.tsx
+++ frontend/src/components/ui/Landing/ui/HeaderLanding/HeaderLanding.tsx
@@ -1,8 +1,38 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import { Space } from 'antd';
+
+import Logo from '../../../../common/Logo';
+
 import './HeaderLanding.scss';
 
 const HeaderLanding: FC = () => {
-  return <header>HeaderLanding</header>;
+  return (
+    <header className="header">
+      <nav className="header__nav">
+        <Logo color="black" />
+        <ul className="header__list">
+          <li className="header__list-item">
+            <a href="">Features</a>
+          </li>
+          <li className="header__list-item">
+            <a href="">Pricing</a>
+          </li>
+          <li className="header__list-item">
+            <a href="">Contact</a>
+          </li>
+          <Space size={12}>
+            <Link to={'/login'} className="link-btn">
+              Log In
+            </Link>
+            <Link to={'/login'} className="link-btn">
+              Sign Up
+            </Link>
+          </Space>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default HeaderLanding;
