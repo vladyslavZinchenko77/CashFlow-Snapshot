9b740c0fa41372708bec667b8b3e1e60389d8850
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2023-12-18 19:48:29 +0200

    updated log in page

--- frontend/src/components/ui/Landing/ui/HeaderLanding/HeaderLanding.tsx
+++ frontend/src/components/ui/Landing/ui/HeaderLanding/HeaderLanding.tsx
@@ -1,12 +1,25 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { Space } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 import Logo from '../../../../common/Logo';
 
 import './HeaderLanding.scss';
 
 const HeaderLanding: FC = () => {
+  const navigate = useNavigate();
+
+  const logIn = () => {
+    let login = localStorage.getItem('login');
+
+    if (login === 'test@gmail.com') {
+      navigate('/dashboard');
+    } else {
+      navigate('/log-in');
+    }
+  };
+
   return (
     <header className="header">
       <nav className="header__nav">
@@ -26,9 +39,9 @@ const HeaderLanding: FC = () => {
             <a href="">Contact</a>
           </li>
           <Space size={12}>
-            <Link to={'/log-in'} className="link-btn">
+            <a href="" onClick={logIn} className="link-btn">
               Log In
-            </Link>
+            </a>
             <Link to={'/sign-up'} className="link-btn">
               Sign Up
             </Link>
