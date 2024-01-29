444283d43f4d37028dd784ef1d6291a4c97bc75b
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-01-29 15:48:19 +0200

    made moneystream common and sorted

--- frontend/src/components/common/MoneyStreamList/MoneyStreamList.tsx
+++ frontend/src/components/common/MoneyStreamList/MoneyStreamList.tsx
@@ -1,19 +1,87 @@
 import { FC } from 'react';
-// import IncomesList from '../IncomesList';
-// import ExpensesList from '../ExpensesList';
 import { useAppContext } from '../../../context/AppContext';
+
+import {
+  ArrowDownOutlined,
+  ArrowUpOutlined,
+  ShoppingCartOutlined,
+  CreditCardOutlined,
+  HomeOutlined,
+  PercentageOutlined,
+  CarOutlined,
+  DollarOutlined,
+} from '@ant-design/icons';
 
 const MoneyStreamList: FC = () => {
   const { incomes, expenses } = useAppContext();
 
   let moneyStream = [...incomes, ...expenses];
 
-  console.log(moneyStream);
+  moneyStream.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
 
   return (
     <div>
       MoneyStreamList
-      <div></div>
+      {moneyStream.map((item, index) => {
+        let icon;
+        if (item.type === 'Продукти') {
+          icon = (
+            <ShoppingCartOutlined
+              style={{ color: 'green', margin: '0 8px 0 8px' }}
+            />
+          );
+        } else if (item.type === 'Комунальні платежі') {
+          icon = (
+            <HomeOutlined style={{ color: 'red', margin: '0 8px 0 8px' }} />
+          );
+        } else if (item.type === 'Заправка автомобіля') {
+          icon = (
+            <CarOutlined style={{ color: 'red', margin: '0 8px 0 8px' }} />
+          );
+        } else if (item.type === 'Здача нерухомості') {
+          icon = (
+            <HomeOutlined style={{ color: 'green', margin: '0 8px 0 8px' }} />
+          );
+        } else if (item.type === 'Заробітня плата') {
+          icon = (
+            <CreditCardOutlined
+              style={{ color: 'green', margin: '0 8px 0 8px' }}
+            />
+          );
+        } else if (item.type === 'Відсотки по депозиту') {
+          icon = (
+            <PercentageOutlined
+              style={{ color: 'green', margin: '0 8px 0 8px' }}
+            />
+          );
+        } else {
+          icon = (
+            <DollarOutlined
+              style={{
+                color: item.title === 'incomes' ? 'green' : 'red',
+                margin: '0 8px 0 8px',
+              }}
+            />
+          );
+        }
+        return (
+          <ul style={{ marginTop: 24, listStyle: 'none' }} key={index}>
+            <li>
+              {item.title === 'incomes' ? (
+                <ArrowDownOutlined style={{ color: 'green' }} />
+              ) : (
+                <ArrowUpOutlined style={{ color: 'red' }} />
+              )}
+              {icon}
+              {item.title === 'incomes' ? '+' : '-'}
+              {item.amount} {'₴'} {''}
+              {item.type}, <strong>Date:</strong> {item.date.toString()}
+            </li>
+          </ul>
+        );
+      })}
     </div>
   );
 };
